9bfdf40a1bb70842f9a4fb71f1b5df5a3309b982
kleyla <kleyla01@live.com>
2020-11-19 22:47:33 -0400

    crud app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import Contador from "./components/Contador";
-import Lista from "./components/Lista";
-import Formulario from "./components/Formulario";
-import FormHook from "./components/FormHook";
-import Ejemplo1 from "./components/Ejemplo1";
-import Saludo from "./components/Saludo";
-import Comentario from "./components/Comentario";
+import React, { useState } from "react";
+import UserTable from "./components/UserTable";
+import { v4 as uuidv4 } from "uuid";
+import AddUserForm from "./components/AddUserForm";
+import EditUserForm from "./components/EditUserForm";
 
 function App() {
-  const sujeto = {
-    nombre: "Karen",
-    url: "https://via.placeholder.com/64",
-    texto:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, quia?",
+  const usersData = [
+    { id: uuidv4(), name: "Tania", username: "floppydiskette" },
+    { id: uuidv4(), name: "Craig", username: "siliconeidolon" },
+    { id: uuidv4(), name: "Ben", username: "benisphere" },
+  ];
+
+  const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState({
+    id: null,
+    name: "",
+    username: "",
+  });
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({
+      id: user.id,
+      name: user.name,
+      username: user.username,
+    });
+  };
+
+  const addUser = (user) => {
+    user.id = uuidv4();
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    // console.log(id);
+    const arrayFiltrado = users.filter((user) => user.id !== id);
+    setUsers(arrayFiltrado);
+  };
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false);
+    setUsers(users.map((user) => (user.id === id ? updateUser : user)));
   };
   return (
-    <div className="container mt-5">
-      <h1>Principal</h1>
-      {/* <Contador /> */}
-      {/* <Lista /> */}
-      {/* <Formulario/> */}
-      {/* <FormHook /> */}
-      {/* <Ejemplo1 /> */}
-      <Saludo persona="Karen" />
-      <Saludo persona="Yolanda" />
-      <Saludo persona="Rocio" />
-      <Comentario sujeto={sujeto} />
+    <div className="container">
+      <h1>CRUD WITH HOOKS</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm currentUser={currentUser} updateUser={updateUser} />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
+        </div>
+        <div className="flex-large">
+          <h2>View user</h2>
+          <UserTable
+            users={users}
+            deleteUser={deleteUser}
+            setEditing={setEditing}
+            editRow={editRow}
+          />
+        </div>
+      </div>
     </div>
   );
 }
